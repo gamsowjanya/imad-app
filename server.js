ec5bafd9a6adfe43dbd8773c33dca7b3d0272d65
gamsowjanya <31132801+gamsowjanya@users.noreply.github.com>
2017-08-22 19:18:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function(req, res){
-    res.sendFile('Article one will be served here');
+  res.sendFile(path.join(__dirname, 'ui', 'Article one.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
